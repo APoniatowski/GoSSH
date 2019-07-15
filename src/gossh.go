b84838743b088692ae27ceeb5e241fda28f8d5fa
APoniatowski <adam.poniatowski@outlook.com>
2019-07-15 22:24:05 +0200

    restructured the yml file and got the parser up, next will be to 'pipe' the info through to ssh, once I am done with that

--- src/gossh.go
+++ src/gossh.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
 
+	"gopkg.in/yaml.v2"
+)
+
+// Error checking function
+func check(e error) {
+	if e != nil {
+		log.Fatalf("error: %v", e)
+	}
+}
+
+// Yaml structs goes here
+type Config struct {
+	ServerList map[string]Servers `yaml:"ServerList"`
+}
+
+type Servers struct {
+	FQDN     string `yaml:"FQDN"`
+	Username string `yaml:"Username"`
+	Password string `yaml:"Password"`
+	Key      string `yaml:"Key"`
+}
+
+// Main function to carry out operations
+func main() {
+	yamlLocation, _ := filepath.Abs("./config/config.yml")
+	configYaml, err := ioutil.ReadFile(yamlLocation)
+	check(err)
+	var configs Config
+	err = yaml.Unmarshal(configYaml, &configs)
+	check(err)
+	fmt.Printf("%+v\n", configs)
 }
