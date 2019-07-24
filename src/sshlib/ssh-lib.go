0afede69bee4be406fde841975b29824f1103bbe
Poniatowski <adam.poniatowski@nokia.com>
2019-07-24 16:20:45 +0200

    ... hmmmmmmmmmm

--- src/sshlib/ssh-lib.go
+++ src/sshlib/ssh-lib.go
@@ -1,4 +1,4 @@
-package ssh-lib
+package sshlib
 
 import ("")  //found the ssh package online... lost the exact name of it, but will add later
 // need to add some args to pass into this func later
