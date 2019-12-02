90d870fb730a2c9010a7c8546cc296dcf27f94c0
APoniatowski <adam.poniatowski@outlook.com>
2019-12-02 03:43:08 +0100

    planning how I am going to tackle this. mostly comments added and a simple check to be able to pass as a argument/parameter in another function in the near future

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ type Config struct {
 	FQDN     string `yaml:"FQDN"`
 	Username string `yaml:"Username"`
 	Password string `yaml:"Password"`
-	Key_Path string `yaml:"Key_Path"`
+	KeyPath  string `yaml:"Key_Path"`
 	Port     string `yaml:"Port"`
 }
 
@@ -31,6 +31,7 @@ type Config struct {
 func main() {
 	var config map[string]map[string]Config
 
+	//TODO This part needs to be in a function... struggling with local/global variables does not function like in rust/python /////////////////////
 	yamlLocation, _ := filepath.Abs("./config/config.yml")
 	bufRead, err := os.Open(yamlLocation)
 	generalError(err)
@@ -42,11 +43,16 @@ func main() {
 	for scanner.Scan() {
 		configYaml = append(configYaml, scanner.Text())
 	}
-	// configYaml, err := ioutil.ReadFile(yamlLocation)
-	parsed := strings.Join(configYaml, "\n")
+	parse := strings.Join(configYaml, "\n")
+	// TODO ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	err = yaml.Unmarshal([]byte(parsed), &config)
+	err = yaml.Unmarshal([]byte(parse), &config)
 
+	//TODO 4 functions will be made of this in ssh-lib, each using anonymous functions and goroutines, which will be chosen via cmdline args
+	//TODO first option: run groups sequentially and servers concurrently
+	//TODO second option: run groups concurrently and servers sequentially
+	//TODO third option: run groups and servers sequentially
+	//TODO fourth option: apocalypse mode, run groups and servers concurrently, execute all in other words
 	for groupKey, groupValue := range config {
 		fmt.Printf("ServerGroup name: %v\n", groupKey)
 		for serverKey, serverValue := range groupValue {
@@ -54,12 +60,16 @@ func main() {
 			fmt.Printf("\t\t%v\n", serverValue.FQDN)
 			fmt.Printf("\t\t%v\n", serverValue.Username)
 			fmt.Printf("\t\t%v\n", serverValue.Password)
-			fmt.Printf("\t\t%v\n", serverValue.Key_Path)
+			fmt.Printf("\t\t%v\n", serverValue.KeyPath)
 			fmt.Printf("\t\t%v\n", serverValue.Port)
+			//! if statement will be needed to make empty ports default to port 22 and passwords to default to ssh keys
+			test := serverValue.FQDN + ":" + serverValue.Port
+			fmt.Printf("%v %T\n", test, test)
 		}
 	}
+	//TODO /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	// fmt.Printf("Result: %v\n", config)
-	// fmt.Printf("Server22 is: %s\n", config["ServerGroup2"]["Server22"])
+	//! fmt.Printf("Result: %v\n", config)
+	//! fmt.Printf("Server22 is: %s\n", config["ServerGroup2"]["Server22"])
 
 }
