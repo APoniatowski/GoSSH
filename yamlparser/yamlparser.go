51d9c85bb970e2688fa5054cbe4395af15e29400
Adam Poniatowski <adam.poniatowski@outlook.com>
2020-02-27 18:22:35 +0100

    added some error handling in the yaml parser and added some color to it.

--- yamlparser/yamlparser.go
+++ yamlparser/yamlparser.go
@@ -9,6 +9,7 @@ import (
 	"runtime"
 	"strings"
 
+	"github.com/gookit/color"
 	"gopkg.in/yaml.v2"
 )
 
@@ -27,36 +28,54 @@ var (
 	ServersPerGroup []int
 )
 
-// func init() {
-// }
 
-// Rollcall Tally up the number of servers and groups and servers in groups
+// Rollcall Parse and tally up the number of servers and groups and servers in groups
 func Rollcall() {
-	fmt.Println("Parsing data...")
+	cyan := color.Cyan.Render
+	green := color.Green.Render
+	red := color.Red.Render
+	yellow := color.Yellow.Render
+	fmt.Println(yellow("Parsing data..."))
 	data := ParseServersList()
 	err := yaml.Unmarshal([]byte(data), &Config)
-	generalError(err)
-
-	fmt.Println("Data parsed, no errors encountered...")
+	if err != nil {
+		fmt.Println(yellow("Data could not be parsed, "), red("encountered some errors..."))
+		fmt.Println(yellow("Please check your pool.yml file, or generate one with:"))
+		fmt.Println(cyan("gossh generate pool template"))
+		fmt.Println(yellow("or display a example with:"))
+		fmt.Println(cyan("gossh generate pool example"))
+	} else {
+		fmt.Println(yellow("Data parsed, "), green("no errors encountered..."))
+	}
 	Waittotal = TotalServercount(Config)
 	ServersPerGroup = ServersPerGroupcount(Config)
 	Grouptotal = len(Config)
-	fmt.Printf("Total number of servers: %d\n", Waittotal)
-	fmt.Printf("Total number of servers per group: ")
+	fmt.Printf(yellow("Total number of servers: %s\n"), Waittotal)
+	fmt.Printf(yellow("Total number of servers per group: "))
 	for _, totalItem := range ServersPerGroup {
-		fmt.Printf(" %d ", totalItem)
+		fmt.Printf(" %s ", totalItem)
 	}
 	fmt.Printf("\n")
-	fmt.Printf("Total groups of servers: %d\n", Grouptotal)
-	fmt.Printf("Total number of logical cores: %v\n", runtime.NumCPU())
-	fmt.Printf("======================================================\n")
+	fmt.Printf(yellow("Total groups of servers: %s\n"), Grouptotal)
+	fmt.Printf(yellow("Total number of logical cores: %s\n"), runtime.NumCPU())
+	fmt.Printf(yellow("======================================================\n"))
 }
 
 // ParseServersList server list parser, parses it to a map of structs in main function
 func ParseServersList() string {
-	yamlLocation, _ := filepath.Abs("./config/pool-testing.yml") // remove -testing to revert changes, using custom server list
+	cyan := color.Cyan.Render
+	// green := color.Green.Render
+	// red := color.Red.Render
+	yellow := color.Yellow.Render
+	yamlLocation, _ := filepath.Abs("./config/pool-testing12.yml") // remove -testing to revert changes, using custom server list
 	bufRead, err := os.Open(yamlLocation)
-	generalError(err)
+	if err != nil {
+		fmt.Println(yellow("Please check your pool.yml file, or generate one with:"))
+		fmt.Println(cyan("gossh generate pool template"))
+		fmt.Println(yellow("or display a example with:"))
+		fmt.Println(cyan("gossh generate pool example"))
+		os.Exit(-1)
+	}
 	defer bufRead.Close()
 
 	scanner := bufio.NewScanner(bufRead)
